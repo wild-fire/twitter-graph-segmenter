7b054c68b0ba2a29bd52c38019ace1d6ab12f312
David J. Brenes <davidjbrenes@gmail.com>
2014-05-25 13:44:28 +0200

    Comments in the code

--- bin/tw-week-user.rb
+++ bin/tw-week-user.rb
@@ -6,15 +6,17 @@ require 'lib/week_segmenter'
 require 'vcr'
 require 'debugger'
 
+# We configure VCR
 VCR.configure do |c|
   c.cassette_library_dir = 'data/vcr'
   c.hook_into :webmock # or :fakeweb
 
+  # We hide the tokens from VCR
   yml_config = YAML.load_file( File.expand_path('../config/twitter.yml', File.dirname(__FILE__)) )['twitter']
   c.filter_sensitive_data('<CONSUMER KEY>') { yml_config['consumer_key'] }
   c.filter_sensitive_data('<ACCESS TOKEN>') { yml_config['access_token'] }
 
-  # Don't playback transient errors
+  # Don't playback errors and rate limits
   c.before_playback do |interaction|
     interaction.ignore! if interaction.request.uri.include? '/1.1/application/rate_limit_status.json?resources=users'
     interaction.ignore! if interaction.response.status.code >= 400 && interaction.response.status.code != 404
@@ -25,18 +27,24 @@ VCR.configure do |c|
 end
 
 program :version, '0.0.1'
-program :description, 'This program segments a period of times  '
+program :description, 'This program Find Twitter last users of the week between two user signup dates'
 
 command :find do |c|
-  c.syntax = 'tw-week-user find username1 username2'
+  c.syntax = 'tw-week-user find username1 username2 [path/to/input/beacons/file]'
   c.summary = 'Find last users of the week between the two users'
   c.description = 'This command takes two users and their signup dates and find the lasr user of the week for each week between the two dates'
   c.example 'Find all the users between "pud" and "goldman" users' , 'find pud goldman'
+  c.example 'Find all the users between "pud" and "goldman" users using beacons' , 'find pud goldman data/beacons.tsv'
   c.action do |args, options|
+
+    # Just to know our rate limit status we print it out
     rate_limit_info = WeekSegmenter.rate_limit_info
     puts "Remaining #{rate_limit_info[:remaining]} of #{rate_limit_info[:limit]} until #{Time.at rate_limit_info[:reset]}"
+
+
     VCR.use_cassette('users_profiles') do
 
+      # Now we get both users
       first_user = WeekSegmenter.client.user args[0]
       last_user = WeekSegmenter.client.user args[1]
 
@@ -58,6 +66,8 @@ command :find do |c|
         puts "#{u.screen_name} signed up at #{u.created_at}"
       end
     end
+
+    # Just to know our rate limit status we print it out
     rate_limit_info = WeekSegmenter.rate_limit_info
     puts "Remaining #{rate_limit_info[:remaining]} of #{rate_limit_info[:limit]} until #{Time.at rate_limit_info[:reset]}"
   end
@@ -69,13 +79,17 @@ command :file do |c|
   c.summary = 'Segments a period of time using users extracted from a TSV file'
   c.description = 'This command takes a file in TSV format with two columns, the user id on twitter and the creation date, ordered by the creation date (sooner first). Then finds the last user of every weeb from the first creation date to the last one'
   c.action do |args, options|
+
+    # Just to know our rate limit status we print it out
     rate_limit_info = WeekSegmenter.rate_limit_info
     puts "Remaining #{rate_limit_info[:remaining]} of #{rate_limit_info[:limit]} until #{Time.at rate_limit_info[:reset]}"
 
+    # We open the The users input file
     tsv_file = File.open args[0]
     previous_user = nil
     next_user = nil
 
+    # And now the beacons file
     if args.length > 2
       beacons_file = File.open args[2]
       beacons = []
@@ -87,17 +101,24 @@ command :file do |c|
     end
 
     VCR.use_cassette('users_profiles') do
+      # Now, for each line of the file
       tsv_file.each_line do |l|
+        # We get the user id and signup date
         user_id, signup_date = l.split("\t")
+        # We store the previous user and create the new one
         previous_user = next_user
         next_user = { user_id: user_id.to_i, signup_date: Date.parse(signup_date) }
 
+        # If there was a previous user (it's not the first file line)
         unless previous_user.nil?
+          # We find the last users of the week
           results = WeekSegmenter.find(previous_user, next_user) do |user|
+            # For each week we save the user into the output file
             output_file = File.open args[1], 'a'
             output_file << "#{user.id}\t#{user.screen_name}\t#{user.created_at}\n"
             output_file.close
 
+            # And store the beacons
             if args.length > 3
               beacons_output_file = File.open args[3], 'a'
               WeekSegmenter.beacons.each do |b|
--- lib/week_segmenter.rb
+++ lib/week_segmenter.rb
@@ -16,6 +16,8 @@ class WeekSegmenter
   end
 
   # Singleton method for getting twitter client according to the configuration in the YML file
+  # WARNING: client must not be stored in a variable and it always must be used directly: WeekSegmenter.client.user(...)
+  # Otherwise, the counter method for controlling the rate limit would be useless
   def self.client
     @@client ||= Twitter::REST::Client.new do |config|
       yml_config = YAML.load_file( File.expand_path('../config/twitter.yml', File.dirname(__FILE__)) )['twitter'].symbolize_keys
@@ -24,24 +26,34 @@ class WeekSegmenter
       config.access_token        = yml_config[:access_token]
       config.access_token_secret = yml_config[:access_token_secret]
     end
+    # Since we can't control when the API is called we decrement the counter here, hoping that when the client is called only 1 API call is made
     @@remaining_calls -= 1
     @@client
   end
 
+  # This method obtains the rate limit info for the users API
   def self.rate_limit_info
     rate_info = WeekSegmenter.client.get '/1.1/application/rate_limit_status.json?resources=users'
     rate_info.body[:resources][:users][:"/users/show/:id"]
   end
 
+  # This method uses the information returned by rate_limit_info to make the whole script sleep until we have enough rate limit again
   def self.sleep_until_rate_limit
+    # Rate limit info calls also have a rate limit, so we use a counter to limit the amount of API calls made
+    # When this counter is less than 5, we make the check
     if @@remaining_calls < 5
+      # We get the rate limit info
       rate_info = rate_limit_info
+      # Since we are using a counter and not perform this check with every API call we must give some space or we can fall into the rate limit without noticing
+      # Our space are 20 API calls. If we have less than 20 we stop
       if rate_info[:remaining] < 20
+        # And now here we sleep until the reset time for the rate limit
         log "Now I'm going to sleep until I have enough rate (#{Time.at rate_info[:reset]} - #{Time.at(rate_info[:reset]) - Time.now} seconds)"
         sleep(Time.at(rate_info[:reset]) - Time.now)
       else
         log "Remaining calls #{rate_info[:remaining]} until #{Time.at rate_info[:reset]}"
       end
+      # We reset the counter now
       @@remaining_calls = [20, rate_info[:remaining]/2].min
     end
   end
@@ -59,7 +71,7 @@ class WeekSegmenter
 
     week = 0
     results = []
-    # From 1 to the numbers of week
+    # While we don't go further the last user signup date...
     while (end_of_week <= last_user[:signup_date]) do
 
       week += 1
@@ -70,7 +82,7 @@ class WeekSegmenter
       user = nil
 
       # We are going to implement a little binary search method
-      # This variable will control which side of the end of the week are we
+      # This variable will control which side of the end of the week we are in
       past_week = false
       # signups_rate will be our increment variable so we are going to decrement it until we find the last user of the week
       signups_rate = global_signups_rate
@@ -81,17 +93,21 @@ class WeekSegmenter
         begin
           log "-- Trying #{guess}"
           user = nil
+          # We try with our local cache "Have we asked about this user before?"
           if @@tried_users.has_key? guess
             user = @@tried_users[guess]
+            # If we asked but the user was nil (didn't exist) then we keep trying
             while user.nil? && @@tried_users.has_key?(guess+signups_rate)
               guess += past_week ? signups_rate : -signups_rate
               user = @@tried_users[guess]
             end
           end
 
+          # If there was no user in our cache we ask to the client
           if user.nil?
             sleep_until_rate_limit
             user = client.user(guess)
+            # And now we save itin the cache and in the beacons array
             @@tried_users[guess] = user
             @@beacons << { user_id: user.id, signup_date: user.created_at }
           end
@@ -102,14 +118,14 @@ class WeekSegmenter
             # If we are still in the past week, then we are going to find another future user
             when past_week && (user.created_at <= end_of_week)
               guess += signups_rate
-            # If we are in the next week, and the user is from the next week, then we get a past user
+            # If we are in the next week, and the user is from the next week, then we get a previous user
             when !past_week && (user.created_at > end_of_week)
               guess -= signups_rate
             # If we are in the past week BUT the user is from the next week, then we cut signups rate
             # in half and search in the oposite direction
             when past_week && (user.created_at > end_of_week)
               # WAIT!! if the user is in the "next" side we can't cut the signups rate to 0 or
-              # it would mean that the last user of a week is in fact the first user of the next one
+              # it would mean that the last user of a week is in the next week (in fact, the first user of the next one)
               signups_rate = (signups_rate/2).ceil unless signups_rate == 1
               past_week = false
               guess -= signups_rate
@@ -120,9 +136,14 @@ class WeekSegmenter
               past_week = true
               guess += signups_rate
           end
+        # We control deleted and private users
         rescue Twitter::Error::NotFound, Twitter::Error::Forbidden
+          # We store in the cache that the user doesn't exist
           @@tried_users[guess] = nil
+
+          # And now move forward (or backwards, depending on the week)
           guess += past_week ? signups_rate : -signups_rate
+
           # If we get lower than one of our beacons from the previous week then turn around and search again
           beacon = if !past_week
             @@beacons.detect{|b| b[:user_id] > guess && b[:signup_date] <= end_of_week }
@@ -130,24 +151,32 @@ class WeekSegmenter
           else
             @@beacons.detect{|b| b[:user_id] < guess && b[:signup_date] > end_of_week }
           end
+
+          # If we have a beacon then we can use it
           unless beacon.nil?
             log "-- User not found but beacon #{beacon[:user_id]} created at #{beacon[:signup_date] } found"
+            # Our guess will be the beacon id, so we fill guess and past_week as they should
             guess = beacon[:user_id]
             past_week = beacon[:signup_date] < end_of_week
+            # We cut signups_rate in half (after all, we have reached a limit)
             signups_rate = (signups_rate/2).ceil
             # WAIT!! if the user is in the "next" side we can't cut the signups rate to 0 or
               # it would mean that the last user of a week is in fact the first user of the next one
             signups_rate = 1 if (signups_rate == 0) && !past_week
           end
+        # Sometimes twitter returns a Timeout
         rescue Twitter::Error::RequestTimeout
         end
       end
 
+      # Here we already have our last week user, so we prepare our next week
       guess += global_signups_rate * 7
       end_of_week += 7.days
 
+      # If a block was given then we yield it
       yield user if block_given?
 
+      # And store this last week user as a result
       results << user
 
     end
